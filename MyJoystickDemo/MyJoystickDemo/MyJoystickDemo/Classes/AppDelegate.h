a80c9df8c6f975bd8f35fd0534151fbf8bf5137b
Daniel Witurna <daniel.witurna@gmail.com>
2014-07-20 13:40:05 +0200

    ZJoystick Version 1.4 - Port to cocos2d 3.x - Minor improvements

---
+++ MyJoystickDemo/MyJoystickDemo/MyJoystickDemo/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  MyJoystickDemo
+//
+//  Created by Daniel Witurna on 20.07.14.
+//  Copyright Daniel Witurna 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ MyJoystickDemo/MyJoystickDemo/MyJoystickDemo/Classes/DemoScene.h
@@ -0,0 +1,27 @@
+//
+//  DemoScene.h
+//  MyJoystickDemo
+//
+//  Created by Daniel Witurna on 20.07.14.
+//  Copyright Daniel Witurna 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface DemoScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (DemoScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
